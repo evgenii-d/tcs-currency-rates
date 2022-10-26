a94c4672636a344d819f3889c879f7982d105c71
Evgenii Dolgii <zerson13@gmail.com>
2022-10-26 12:03:05 +0300

    Fix message output

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import sched
 import time
 import logging
-from random import randrange
+from random import choice, randrange
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s | %(levelname)s | %(message)s')
@@ -20,14 +20,15 @@ except FileNotFoundError:
 CURRENCIES = CONFIG['currencies']
 BOT_TOKEN = CONFIG['botToken']
 CHAT_ID = CONFIG['chatID']
-TIMEOUT = CONFIG['timeout']
-SHED = sched.scheduler(time.time, time.sleep)
+INTERVAL = CONFIG['interval']
+SPREAD = CONFIG['spread']
+SCHED = sched.scheduler(time.time, time.sleep)
 
 
-def send_currency_rates():
+def get_currency_rates(currencies) -> str:
     result = 'Debit Cards Transfers\n'
 
-    for currency in CURRENCIES:
+    for currency in currencies:
         with urllib.request.urlopen(currency) as data:
             data = json.loads(data.read())
 
@@ -35,24 +36,37 @@ def send_currency_rates():
                 if item['category'] == 'DebitCardsTransfers':
                     from_currency = item['fromCurrency']['name']
                     to_currency = item['toCurrency']['name']
-                    result += f"\n{from_currency} | {to_currency}\nBUY: {item['buy']} | SELL: {item['sell']}\n"
+                    if from_currency == 'RUB':
+                        result += f"\n1 {from_currency} = {item['buy']} {to_currency}\nBUY: {item['buy']} | SELL: {item['sell']}\n"
+                    else:
+                        result += f"\n1 {from_currency} = {item['sell']} {to_currency}\nBUY: {item['sell']} | SELL: {item['buy']}\n"
+    return result
 
-    result = urllib.parse.quote(result)
-    bot_api = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={result}'
+
+def send_message(bot_token: str, chat_id: str, bot_message: str) -> None:
+    bot_api = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={bot_message}'
 
     with urllib.request.urlopen(bot_api) as message:
         data = json.loads(message.read())
         logging.info(f"Message sent: {data['ok']}")
 
 
-def shedHandler():
-    send_currency_rates()
-    SHED.enter(TIMEOUT + randrange(300), 1, shedHandler)
+def shedHandler(prev_bot_message) -> None:
+    bot_message = get_currency_rates(CURRENCIES)
+    delay = INTERVAL + randrange(SPREAD) * choice([-1, 1])
+
+    if bot_message != prev_bot_message:
+        send_message(BOT_TOKEN, CHAT_ID, urllib.parse.quote(bot_message))
+        prev_bot_message = bot_message
+    else:
+        logging.info('No changes')
+
+    SCHED.enter(delay, 1, shedHandler, argument=(prev_bot_message,))
 
 
 try:
     logging.info(f'CurrencyRates script started')
-    SHED.enter(1, 1, shedHandler)
-    SHED.run()
+    SCHED.enter(1, 1, shedHandler, argument=('',))
+    SCHED.run()
 except KeyboardInterrupt:
     pass
